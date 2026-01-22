1914fbb350a29ac05166e5c2d8ea0a2ee9100108
Hisaw <hisawsarah321@gmail.com>
2026-01-22 04:19:09 -0800

    heroside fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import heroBg from "../assets/hero.jpg";
-import heroSide from "../assets/hero-side.png";
+import heroSide from "../assets/heroside.png";
 const Hero = () => {
   return (
     <section
